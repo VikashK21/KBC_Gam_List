36cb44b6c0c838109b1c06fe4b9b55c23e3a8483
Vikash Kumar <87763352+VikashK21@users.noreply.github.com>
2021-08-02 11:12:17 +0530

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -45,21 +45,3 @@ if i==(len(q)-1):
     print (" You are the Crorepati of this Game. We KBC team wish that  you'll use these amounts in fulfilling your dream 😃😇 ")
 
 
-
-
-
-
-# n=0
-# a=["RAJASTHAN"]
-# b=["+------+\n|\n|\n|\n|\n|\n|\n|\n|______\n","\n+------+\n|      |\n|      O\n|\n|\n|\n|\n|\n|\n|______","\n+------+\n|      |\n|      O\n|      |\n|      |\n|\n|\n|\n|\n|______","\n+------+\n|      |\n|      O\n|     /|      \n|      |\n|\n|\n|\n|\n|______","\n+------+\n|      |\n|      O\n|     /|\      \n|      |\n|\n|\n|\n|\n|______","\n+------+\n|      |\n|      O\n|     /|\      \n|      |\n|     /\n|\n|\n|\n|______","\n+------+\n|      |\n|      O\n|     /|\      \n|      |\n|     / \ \n|\n|\n|______"]       
-# print("     **HANGMAN**\n\nGuess a state of India.\n")
-# while n<=6:
-#      choice=input("Enter a state name:- ")
-#      if choice in list(a):
-#          print('\nYou Won the Game.')
-#          break
-#      elif choice not in list(a):
-#          print(b[n])
-#          if n==6:
-#              print('\nSorry You lost the Game.')
-#      n+=1
